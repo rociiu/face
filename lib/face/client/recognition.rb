79816664fb0f83e8d1935a4a0ab1606e93666440
rociiu <rociiu.yu@gmail.com>
2011-04-01 19:03:02 +0800

    fix params in recognition, update version

--- lib/face/client/recognition.rb
+++ lib/face/client/recognition.rb
@@ -3,21 +3,22 @@ module Face
     module Recognition
       # http://api.face.com/faces/detect.json
       def faces_detect(opts={})
-        opts.assert_valid_keys(:urls)
+        opts.assert_valid_keys(:urls, :file, :detector, :attributes, :callback, :callback_url)
         make_request(:faces_detect, opts)
       end
 
       def faces_recognize(opts={})
-        opts.assert_valid_keys(:uids, :urls)
+        opts.assert_valid_keys(:uids, :urls, :namespace, :detector, :attributes, :callback, :callback_url)
         make_request(:faces_recognize, opts.merge(user_auth_param))
       end
 
       def faces_train(opts={})
-        opts.assert_valid_keys(:uids)
+        opts.assert_valid_keys(:uids, :namespace, :callback, :callback_url)
         make_request(:faces_train, opts.merge(user_auth_param))
       end
 
       def faces_status(opts={})
+        opts.assert_valid_keys(:uids, :namespace, :callback, :callback_url)
         make_request(:faces_status, opts.merge(user_auth_param))
       end
     end
