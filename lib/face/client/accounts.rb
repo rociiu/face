ea316aa098f3dce8bb6146d48741bffbfdbe6e12
TJ Singleton <tjsingleton@me.com>
2014-04-26 20:24:56 -0400

    account_limits was using wrong key

--- lib/face/client/accounts.rb
+++ lib/face/client/accounts.rb
@@ -2,7 +2,7 @@ module Face
   module Client
     module Accounts
       def account_limits(opts={})
-        make_request(:tags_remove, opts.merge(user_auth_param))
+        make_request(:account_limits, opts.merge(user_auth_param))
       end
 
       def account_users(opts={})
